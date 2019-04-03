ee98caaa0245efda180d813916649ce2ed4b9163
izual1874 <sunandh@163.com>
2019-04-03 14:17:45 +0800

    API&oauth 签名

--- API&oauth/README.md
+++ API&oauth/README.md
@@ -5,3 +5,36 @@
 #2/4-19
 - 目前已经掌握，签名方法
 - 但是，key 藏在哪里，没找到
+
+#3/4-19
+- 继续补全文档
+- oauth 1.0 验证流程
+    - 请求的连接，包含 get/post ，但不包含 oauth_signature， 经过quote 变为ase_str
+    - HAMC-SHA1: 对base_str 加密。HMAC-SHA1 , 中间需要KEY。该码 经过 HEX() 转 16进制。
+        ```python
+        h_sha1 = Hmac_Sha1(sha1_key, base_str)
+        print(h_sha1)
+        print(len(h_sha1))
+        ```
+        ```python
+        b51dfe4f2f3594c79802bfc589dd2431361afd63
+        40
+        ```
+    - hmacsha1_urlencode: urlencode 转换会比之前增长 30% ，这里需要 base_64 编码。重点，编码前需要由16转为2进制. 然后在依次 urlencode编码。
+        ```python
+        print(to_2)
+        b'\xb5\x1d\xfeO/5\x94\xc7\x98\x02\xbf\xc5\x89\xdd$16\x1a\xfdc'
+        print(to_base64)
+        b'tR3+Ty81lMeYAr/Fid0kMTYa/WM='
+        ```
+        ```python
+        url_h_sha1 = hmacsha1_urlencode(h_sha1)
+        print(url_h_sha1)
+        print(len(url_h_sha1))
+        ```
+        ```python
+        tR3%2BTy81lMeYAr/Fid0kMTYa/WM%3D
+        32
+        ```
+    - urlencode_hmacsha1 : urlencode 编码 解码为 HMAC-SHA1
+
---
+++ API&oauth/URL_TO_SHA.py
@@ -0,0 +1,33 @@
+from urllib import parse
+import base64
+import hmac, hashlib
+
+base_str = 'GET&http%3A%2F%2Fphotos.example.net%2Fphotos&file%3Dvacation.jpg%26oauth_consumer_key%3Ddpf43f3p2l4k3l03%26oauth_nonce%3Dkllo9940pd9333jh%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1191242096%26oauth_token%3Dnnch734d00sl2jdk%26oauth_version%3D1.0%26size%3Doriginal'
+sha1_key = 'kd94hf93k423kf44&pfkkdhi9sl3r4s00'
+oauth_signature="tR3%2BTy81lMeYAr%2FFid0kMTYa%2FWM%3D"
+
+
+# get HMAC-SHA1
+def Hmac_Sha1(key, base_str):
+    import hmac, hashlib
+    key = key.encode('utf8')
+    base_str = base_str.encode('utf8')
+    Hmac = hmac.new(key, base_str, hashlib.sha1)
+    Hmac = Hmac.digest()
+    return Hmac.hex()
+
+def hmacsha1_urlencode(hmacsha1):
+    to_2 = bytes.fromhex(hmacsha1)
+    to_base64 = base64.b64encode(to_2)
+    to_urlencode = parse.quote(to_base64)
+    return to_urlencode
+
+def urlencode_hmacsha1(encode_str):
+    un_url = parse.unquote(encode_str)
+    un_base64 = base64.b64decode(un_url.encode('utf8'))
+    to_hex = un_base64.hex()
+    return to_hex
+
+a = '%2F9aBPAqTZ9pOxNtRmkDJ8Ci111A%3D'
+
+print(urlencode_hmacsha1(a))
